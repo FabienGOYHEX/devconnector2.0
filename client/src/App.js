6ffbb683783def128c958933ff2a53edfc571574
fabiengoyhex <fabien.goyhex@gmail.com>
2022-02-02 11:57:33 +0100

    register Form

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
-    <Fragment>
-      <Navbar />
-      <Landing />
-    </Fragment>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Routes>
+      </Fragment>
+    </Router>
   )
 }
 
